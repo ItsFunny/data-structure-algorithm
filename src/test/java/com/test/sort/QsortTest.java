162404a72bbf408b815093f81c479690a4b0a040
joker <18757883747@163.com>
2019-01-20 10:11:04 +0800

    add qsort

--- src/test/java/com/test/sort/QsortTest.java
+++ src/test/java/com/test/sort/QsortTest.java
@@ -1,6 +1,6 @@
 package com.test.sort;
 
-import com.basic.sort.HeapSort;
+import com.basic.sort.QSort;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,28 +9,22 @@ import org.junit.Test;
  * @When
  * @Description
  * @Detail
- * @date 创建时间：2019-01-17 23:26
+ * @date 创建时间：2019-01-18 22:21
  */
-public class HeapSortTest
+public class QsortTest
 {
     private Integer[] normalTestArr = new Integer[]{1, 5, 2, 0, -1, -2, 6, 4, 3};
     private Integer[] successArray = new Integer[]{-2, -1, 0, 1, 2, 3, 4, 5, 6};
 
     @Test
-    public void name()
+    public void normalQsortTest()
     {
-
-    }
-
-    @Test
-    public void normalTestHeapSort()
-    {
-        HeapSort heapSort = new HeapSort();
-        heapSort.heapSort(normalTestArr);
+        QSort qSort=new QSort();
+        qSort.qSort(normalTestArr);
         for (Integer integer : normalTestArr)
         {
             System.out.printf("%d->", integer);
         }
-        Assert.assertArrayEquals(successArray, normalTestArr);
+        Assert.assertArrayEquals(successArray,normalTestArr);
     }
 }
