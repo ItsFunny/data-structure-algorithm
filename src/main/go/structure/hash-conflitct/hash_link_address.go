8b3d622a2a4fb3f9086f0f7cfc68373e05eb8eab
lvcong <18757883747@163.com>
2019-01-29 15:20:38 +0800

    add hashmap

--- src/main/go/structure/hash-conflitct/hash_link_address.go
+++ src/main/go/structure/hash-conflitct/hash_link_address.go
@@ -1,5 +1,11 @@
 package hash_conflitct
 
+import (
+	"azure-container-networking/npm/util"
+	"fmt"
+	"strconv"
+)
+
 /*
 	解决hash冲突的第二种方法:链地址法
  */
@@ -38,6 +44,59 @@ func (l *LinkedList) PopNode() interface{} {
 type HashMap struct {
 	elements []*LinkedList
 	size int
+	capability int
 	loadFactor float32
+	threshold int
+}
+
+func (m *HashMap)Put(value interface{}){
+	if nil==value {
+		return
+	}
+	hashString := util.Hash(fmt.Sprintf("%v", value))
+	hashCode, _ :=strconv.Atoi(hashString)
+	index:=hashCode&(len(m.elements)-1)
+	rootNode:=m.elements[index]
+	newNode:=&LinkNode{
+		data: value,
+	}
+	if nil==rootNode{
+		m.elements[index]=&LinkedList{
+			head: newNode,
+			tail: newNode,
+			size: 1,
+		}
+	}else{
+		rootNode.tail.next=newNode
+		rootNode.tail=newNode
+	}
+	m.size++
+	if m.size> m.threshold{
+		// resize
+	}
+}
+
+func (m *HashMap)resize(){
+	oldCapability:=m.capability
+	//oldEles:=m.elements
+	oldThreshold:=m.threshold
+	//newThreshold:=0
+
+
+	// 重新建立阈值
+	if oldCapability >0 &&(oldThreshold<<1)<2^32-1{
+		newThreshold=oldCapability<<1
+	}
+	// 重新计算hash,然后复制,如果从头到尾遍历计算hash的话太慢了
+}
+
+
+func NewHashMap(maxEles int,loadFactor float32)*HashMap{
+	hashMap:=&HashMap{
+		elements:   make([]*LinkedList,0),
+		size:       0,
+		loadFactor: loadFactor,
+	}
+	return hashMap
 }
 
