4329d1505e91338a374ecd31cdc9f3987e8a18d0
lvcong <18757883747@163.com>
2019-01-30 15:11:25 +0800

    add get loop node

--- src/main/go/algorithm/list_algorithm/loop_list.go
+++ src/main/go/algorithm/list_algorithm/loop_list.go
@@ -52,3 +52,58 @@ func (l *LinkedList)ValidIfLoop()bool{
 		}
 	}
 }
+
+// 通过快慢指针获取回环的节点
+// 遇到的坑: 当break的时候代表发生了碰撞,因为我采用的方法,初始化的时候快节点会比慢节点快一步,所以当break获取
+// 回环节点的时候也需要先快一步
+func (l *LinkedList)GetLoopNode()*ListNode{
+	// 这个方法的测试的目标是:必有回环的链表
+	if l.size==0{
+		return nil
+	}
+	slowNode:=l.root
+	quickNode:=slowNode.next
+	for{
+		slowNode=slowNode.next
+		quickNode=quickNode.next
+		if nil!=quickNode {
+			quickNode=quickNode.next
+		}
+		if nil==quickNode {
+			return nil
+		}else if quickNode==slowNode{
+			// 说明有环碰撞了
+			break
+		}else{
+			slowNode=slowNode.next
+			quickNode=quickNode.next
+			if nil!=quickNode{
+				quickNode=quickNode.next
+			}
+		}
+	}
+	// 如果发生碰撞,则慢节点从表头出发,快节点从碰撞处出发,两者的运动速率是一致的,如果2者相等就是回环的节点
+	// 这里有一个坑,因为最开始quickNode就比slowNode快一步(初始化的时候就快一步,所以这里也需要先提前快一步)
+	slowNode=l.root
+	quickNode=quickNode.next
+	for quickNode!=slowNode{
+		slowNode=slowNode.next
+		quickNode=quickNode.next
+	}
+	return slowNode
+}
+
+
+// 通过map 额外申请内存来判断是否存在环
+func (l LinkedList)ValidIfLoopByMap()bool{
+	validMap:=make(map[*ListNode]struct{})
+	tempNode:=l.root
+	for nil!=tempNode{
+		if _,ok:=validMap[tempNode] ;!ok{
+			validMap[tempNode]= struct{}{}
+		}else{
+			return true
+		}
+	}
+	return false
+}
